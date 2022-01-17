38129c0f284e9c9adb97f464980bcd3258465aac
bubblech <1pcpcpc1pc@gmail.com>
2022-01-17 18:19:20 +0300

    fixed curl url

--- vim.py
+++ vim.py
@@ -23,7 +23,7 @@ def install_vimplug():
     if ISWINDOWS:
         cmd = "iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |` ni $HOME/vimfiles/autoload/plug.vim -Force"
     else:
-        cmd = "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+        cmd = "curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
     system(cmd)
 
 
